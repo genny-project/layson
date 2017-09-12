6b9976a924864bfc6ed26ec9fc0e0a8d2773fbb4
Loris Campanile <campanile.loris@gmail.com>
2017-09-12 21:33:52 +1000

    dumb component

---
+++ dev/ButtonComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var Button = React.createClass({
+
+  render: function () {
+    return (
+      <button class="btn btn-default">Button</button>
+    );
+  }
+});
+
+module.exports = Button;
