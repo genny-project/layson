9802213de360a0288b3d73bee1589356f2525e7a
matthaywardwebdesign <matt@matthayward.com.au>
2017-09-15 14:45:37 +1000

    Exported components from index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-export default {};
+export { ComponentCollection } from './component-collection';
+export { Grid } from './grid';
+export { JSONLoader } from './json-loader';
+export { Repeater } from './repeater';
