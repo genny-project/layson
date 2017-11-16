0f1984eeea001797cd8c6fd394713a289605e9cf
Loris Campanile <loris1634@gmail.com>
2017-11-17 09:35:38 +1100

    grid fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { ComponentCollection } from './component-collection';
-export { Grid } from './grid';
+export { Grid } from './Grid';
 export { JSONLoader } from './json-loader';
 export { Repeater } from './repeater';
